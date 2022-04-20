2dc33ec1a68b0919c563d58c6fabf3118d5cc395
Chron_2020 <10555463+chron-two-thousand-and-twenty@user.noreply.gitee.com>
2022-04-20 20:26:47 +0800

    fix the bug of dijstra and solve the question about how to get the shortest route of two exact point

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include <xmath.h>
 
 typedef struct data
 {
     int start;
     int end;
-    double length;
+    float length;
 }data;
 
-double matrix[5000][5000];
+float matrix[5000][5000];
 data Data[5000];
-int prev[5000];
-double dist[5000];
+float dist[5000];
 int ID_origin[5000],ID_normal[5000];//ID_origin is to store nodes that have not been reassigned
 // ID_normal is to store nodes that have been reassigned
 int sum = 0,num = 0;//sum is the number of edges and num is the number of nodes
@@ -25,10 +23,10 @@ void loadmap(char *filename){
     while(fgets(buf,1024,f)){
         if(strstr(buf,"link id")){
             sscanf(buf,"<link id=%s node=%s node=%s way=%s length=%s veg=%s arch=%s land=%s POI=%s;/link>",&a1,&a2,&a3,&a4,&a5,&a6,&a7,&a8,&a9);
+            sum++;
             Data[sum].start = atoi(a2);
             Data[sum].end = atoi(a3);
             Data[sum].length = atof(a5);
-            sum++;
         }
     }
     fclose(f);
@@ -36,15 +34,15 @@ void loadmap(char *filename){
     while(fgets(buf,1024,f)){
         if(strstr(buf,"node id")){
             sscanf(buf,"<node id=%s lat=%s lon=%s /node>",&a10,&a11,&a12);
-            ID_origin[num] = atoi(a10);
             num++;
+            ID_origin[num] = atoi(a10);
         }
     }
     fclose(f);
     int tmp;
-    for(int i = 0;i<num;i++){
+    for(int i = 1;i<=num;i++){
         tmp = i;
-        for(int j = i + 1;j<num;j++){
+        for(int j = i + 1;j<=num;j++){
             if(ID_origin[j]<ID_origin[tmp]){
                 tmp = j;
             }
@@ -55,79 +53,85 @@ void loadmap(char *filename){
             ID_origin[tmp] = temp;
         }
     }
-    for(int i = 0;i < num;i++){
+    for(int i = 1;i <= num;i++){
         ID_normal[i] = i;
+//        printf("%d\t%d\n",ID_origin[i],ID_normal[i]);
     }
-    for(int i = 0;i < sum;i++){
-        for(int j = 0;j < num;j++){
-            if(Data[i].start == ID_origin[j]){
+    for(int i = 1;i <= sum;i++){
+        int flag1 = 0,flag2 = 0;
+        for(int j = 1;j <= num;j++){
+            if(Data[i].start == ID_origin[j] && flag1 == 0){
                 Data[i].start = ID_normal[j];
+                flag1 = 1;
             }
-            if(Data[i].end == ID_origin[j]){
+            if(Data[i].end == ID_origin[j] && flag2 == 0){
                 Data[i].end = ID_normal[j];
+                flag2 = 1;
             }
         }
     }
-    for(int i = 0;i < 5000;i++){
-        for(int j = 0;j < 5000;j++){
-            matrix[i][j] = INF;
+    for(int i = 1;i < 5000;i++){
+        for(int j = 1;j < 5000;j++){
+            matrix[i][j] = 100000;
         }
     }
-    for(int i = 0;i<sum;i++){
+    for(int i = 1;i<=sum;i++){
         matrix[Data[i].start][Data[i].end] = Data[i].length;
         matrix[Data[i].end][Data[i].start] = Data[i].length;
     }
-    /*for(int i = 0;i < 5000;i++){
-        for(int j = 0;j < 5000;j++){
-            if(matrix[i][j] != 0) {
-                printf("%d\t%d\t%f\n",i,j,matrix[i][j]);
-            }
-        }
-    }*/
 }
 
-
-void dijkstra(int vs) {
-    double min,tmp;
-    int k = 0;
+float dijkstra(int start,int end)
+{
     int flag[5000];
-     for (int i = 0; i < num; i++) {
+    for(int i = 1;i <= num; i++){
+        dist[i] = 100000;
         flag[i] = 0;
-        prev[i] = 0;
-        dist[i] = matrix[vs][i];
     }
-    flag[vs] = 1;
-    dist[vs] = 0;
-    for (int i = 1; i < num; i++) {
-        min = INF;
-        for (int j = 0; j < num; j++) {
-            if (flag[j] == 0 && dist[j] < min) {
-                min = dist[j];
-                k = j;
+    dist[start]=0;
+    for(int i = 0; i <num; i++)
+    {
+        int t = -1;
+        for(int j = 1; j <= num; j++) {
+            if (flag[j] == 0 && (t == -1 || dist[t] > dist[j])) {
+                t = j;
             }
         }
-        flag[k] = 1;
-        for (int j = 0; j < num; j++) {
-            if(matrix[k][j] == 0){
-                tmp = INF;
-            } else{
-                tmp = min + matrix[k][j];
-            }
-            if (flag[j] == 0 && (tmp < dist[j])) {
-                dist[j] = tmp;
-                prev[j] = k;
+        flag[t] = 1;
+        for(int j = 1; j <= num; j++) {
+            if(dist[j] > (dist[t] + matrix[t][j])){
+                dist[j] = dist[t] + matrix[t][j];
             }
         }
     }
+    if(dist[end] == 100000) return 0;
+    return dist[end];
 }
 
 int main(void){
     loadmap("Final_Map.map");
-    printf("dijkstra(%d): \n", ID_normal[0]);
-    dijkstra(0);
-    for (int i = 0; i < num; i++){
-        if(dist[i] != INF) {
-            printf("  shortest(%d, %d)=%f\n", ID_normal[0], ID_normal[i], dist[i]);
+    int end,start;
+    for(int j = 1;j<=5000;j++){
+        end = j;
+        start = 765;
+        double test = dijkstra(start,end);
+        for(int i = 1;i < 5000;i++){
+            if(start == ID_normal[i]){
+                start = ID_origin[i];
+                break;
+            }
+        }
+        for(int i = 1;i < 5000;i++){
+            if(end == ID_normal[i]){
+                end = ID_origin[i];
+                break;
+            }
+        }
+        if(test != 0){
+            printf("%d ---> %d:%f\n",start,end,test);
+        }
+        else{
+            printf("%d ---> %d:no\n",start,end);
         }
     }
 }
